c3a9c0b21811ebc6d43f36438a96fc35014e0e9f
elene kiknadze <ekiknadze@gmail.com>
2023-07-04 11:28:33 +0200

    Add testing mocks to main component

--- src/app/components/main-page/main-page.component.spec.ts
+++ src/app/components/main-page/main-page.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialog } from '@angular/material/dialog';
 import { ResolverService } from '../../services/resolver.service';
 import { AuthService } from '../../services/auth.service';
-import { ScoreboardService } from '../../services/scoreboard.service';
 
 import { MainPageComponent } from './main-page.component';
+import { SCOREBOARD_SERVICE_TOKEN } from '../../services/scoreboard.abstract.service';
+import { NUMBER_OF_SCORES } from '../../tokens/number-of-scores.token';
+import { TimerService } from '../../services/timer.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const MAT_DIALOG_MOCK = {
   open: () => ({
@@ -17,26 +19,38 @@ describe('MainPageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [MainPageComponent],
+      //figure out why I need BrowserAnimationsModule here
+      imports: [MainPageComponent, BrowserAnimationsModule],
       providers: [
         {
           provide: AuthService,
           useValue: { player: 'x', computer: 'o', name: 'someName' },
         },
         {
-          provide: ScoreboardService,
-          useValue: { addScore: jest.fn() },
+          provide: SCOREBOARD_SERVICE_TOKEN,
+          useValue: {
+            addScore: jest.fn(),
+            calculateScore: jest.fn().mockReturnValue(100),
+          },
         },
-        // {
-        //   provide: MatDialog,
-        //   useValue: MAT_DIALOG_MOCK,
-        // },
+        {
+          provide: NUMBER_OF_SCORES,
+          useValue: 10,
+        },
         ResolverService,
+        {
+          provide: TimerService,
+          useValue: {
+            stopCounter: jest.fn(),
+            startCounter: jest.fn(),
+            count: 10,
+          },
+        },
       ],
     });
     fixture = TestBed.createComponent(MainPageComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.autoDetectChanges();
   });
 
   it('should create', () => {
